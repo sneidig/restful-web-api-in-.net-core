bbca44e9975d0fbefd80da602b6bf29ff82fc9fd
Scott Neidig <sneidig@protonmail.com>
2026-06-21 21:20:59 -0600

    Add data migrations to update a ApplicationDbContext field name.

---
+++ RoyalVilla_API/RoyalVilla_API/Migrations/20260622025553_UpdateAppDbContextVillaFieldName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoyalVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAppDbContextVillaFieldName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Villas",
+                table: "Villas");
+
+            migrationBuilder.RenameTable(
+                name: "Villas",
+                newName: "Villa");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Villa",
+                table: "Villa",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Villa",
+                table: "Villa");
+
+            migrationBuilder.RenameTable(
+                name: "Villa",
+                newName: "Villas");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Villas",
+                table: "Villas",
+                column: "Id");
+        }
+    }
+}
