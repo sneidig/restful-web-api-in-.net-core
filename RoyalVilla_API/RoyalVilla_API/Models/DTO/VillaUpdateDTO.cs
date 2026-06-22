07635a7971a2c9af75666ce6065833fef1d2f73b
Scott Neidig <sneidig@protonmail.com>
2026-06-21 22:16:06 -0600

    Add range validation to VillaUpdateDTO numeric fields

--- RoyalVilla_API/RoyalVilla_API/Models/DTO/VillaUpdateDTO.cs
+++ RoyalVilla_API/RoyalVilla_API/Models/DTO/VillaUpdateDTO.cs
@@ -4,14 +4,20 @@ namespace RoyalVilla_API.Models.DTO
 {
     public class VillaUpdateDTO
     {
-        [Required]
+        [Range(1, int.MaxValue)]
         public int Id { get; set; }
         [MaxLength(50)]
         [Required]
         public required string Name { get; set; }
         public string? Details { get; set; }
+
+        [Range(0, 999.99)]
         public decimal Rate { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int Sqft { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
 
